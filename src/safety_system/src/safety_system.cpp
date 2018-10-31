a105756d27d6b530b3cb4dce940c3134066e19f7
Brian Bugert <coolbots7@gmail.com>
2018-10-31 00:26:17 -0700

    Start digital estop service server

--- src/safety_system/src/safety_system.cpp
+++ src/safety_system/src/safety_system.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <diagnostic_msgs/DiagnosticStatus.h>
 #include <ros/console.h>
+#include <std_srvs/Trigger.h>
 
 void print(ros::console::Level level, const std::string& s) {
         ROS_LOG(level, ROSCONSOLE_DEFAULT_NAME, "%s", s.c_str());
@@ -12,13 +13,18 @@ class SafetySystem {
 		void update();
 
 	private:
+		bool digitalEStopServiceCallback(std_srvs::TriggerRequest& request, std_srvs::TriggerResponse& response);
 		ros::NodeHandle nh_;
 
 		ros::Publisher status_pub_;
+
+		ros::ServiceServer digital_estop_service_;
 };
 
 SafetySystem::SafetySystem() {
 	status_pub_ = nh_.advertise<diagnostic_msgs::DiagnosticStatus>("/safety_system", 1);
+
+	digital_estop_service_ = nh_.advertiseService("digital_estop", &SafetySystem::digitalEStopServiceCallback, this);
 }
 
 void SafetySystem::update() {
@@ -30,6 +36,12 @@ void SafetySystem::update() {
 	status_pub_.publish(status);
 
 	print(ros::console::Level::Info, "ESTOP OK");
+}
+
+bool SafetySystem::digitalEStopServiceCallback(std_srvs::TriggerRequest& request, std_srvs::TriggerResponse& response) {
+	response.success = true;
+	response.message = "hi";
+	return 0;
 }
 
 int main(int argc, char** argv) {
